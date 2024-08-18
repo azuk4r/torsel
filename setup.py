cd1d4d9b9d1e6b259a88ed0eaa6a0265ef2f3e95
azuk4r <102126589+azuk4r@users.noreply.github.com>
2024-08-18 21:58:01 +0200

    v0.1.1 - setup.py update for PyPI release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="torsel",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python module for managing Tor instances with Selenium",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
