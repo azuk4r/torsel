5fe056d5cd36f98d378fe616cf8692613ac602e4
azuk4r <102126589+azuk4r@users.noreply.github.com>
2024-08-26 00:55:58 +0200

    v0.3.1 - torsel.py - import fixed

--- torsel/torsel.py
+++ torsel/torsel.py
@@ -14,7 +14,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from cookies_manager import CookiesManager
+from .cookies_manager import CookiesManager
 
 class Torsel:
     """
